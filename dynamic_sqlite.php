d5f718ae10955f751596eae31684d9dd50892b76
pyliu <3634168+pyliu@users.noreply.github.com>
2020-10-14 09:57:10 +0800

    Update dynamic_sqlite.php
    style format

--- dynamic_sqlite.php
+++ dynamic_sqlite.php
@@ -4,82 +4,70 @@
  * @author Christian Tang
  */
 
-class SQLite
-{
+class SQLite {
     private $database;
     private $databaseName;
-    
-    
-    public function __construct($databaseName)
-    {
+
+
+    public function __construct($databaseName) {
         $this->databaseName = $databaseName;
     }
-    
-    public function initdatabase()
-    {
-        try
-        {
-            $this->database = new PDO('sqlite:'.$this->databaseName);
+
+    public function initdatabase() {
+        try {
+            $this->database = new PDO('sqlite:' . $this->databaseName);
             $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        catch(PDOException $e)
-        {
+        } catch (PDOException $e) {
             die($e->getMessage());
         }
     }
-    
-    function createTable($table)
-    {
-        $sTable = 'CREATE TABLE IF NOT EXISTS '.$table->name.' (';
-        $sFields;
-        foreach($table->fields as $f) {
-            if($sFields != '')
-            $sFields .= ',';
-            $sFields .= $f->name.' '.$f->type;
+
+    function createTable($table) {
+        $sTable = 'CREATE TABLE IF NOT EXISTS ' . $table->name . ' (';
+        $sFields = '';
+        foreach ($table->fields as $f) {
+            if ($sFields != '') {
+                $sFields .= ',';
+            }
+            $sFields .= $f->name . ' ' . $f->type;
         }
         $sFields .= ')';
         $sTable .= $sFields;
-                
+
         $this->database->exec($sTable);
     }
-    
-    function insert($table, $array)
-    {
-        try 
-        {
+
+    function insert($table, $array) {
+        try {
             $sColumns = implode(',', array_keys($array));
             $sValues = implode(',', array_fill(0, count($array), '?'));
 
             $stmt = $this->database->prepare("INSERT INTO $table ({$sColumns}) VALUES ({$sValues})");
             return $stmt->execute(array_values($array));
-        }
-        catch(PDOException $e)
-        {
+        } catch (PDOException $e) {
             die($e->getMessage());
         }
     }
 }
 
-class SQLiteTable
-{
+class SQLiteTable {
     var $name;
     var $fields;
-    
+
     public function __construct($tableName) {
         $this->name = $tableName;
     }
-    
+
     public function addField($fieldName, $fieldType) {
         $field = new SQLiteField($fieldName, $fieldType);
         $this->fields[] = $field;
     }
 }
 
-class SQLiteField
-{
+class SQLiteField {
     var $name;
     var $type;
-    
+
     public function __construct($name, $type) {
         $this->name = $name;
         $this->type = $type;
